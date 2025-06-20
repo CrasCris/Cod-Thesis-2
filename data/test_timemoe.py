f68ca92a4e0c08e10c378e65cd1bc112190caf6c
opterix <daedrobu87@gmail.com>
2025-06-20 10:31:08 -0500

    Added test

---
+++ data/test_timemoe.py
@@ -0,0 +1,29 @@
+import random
+from Time_MoE.time_moe.datasets.time_moe_dataset import TimeMoEDataset
+
+
+ds = TimeMoEDataset('Time-300B/healthcare/hospital', normalization_method='max')
+print("Numero de secuencias:", len(ds))
+assert len(ds) == 767
+
+ds = TimeMoEDataset('Time-300B/healthcare/covid_deaths', normalization_method='max')
+assert len(ds) == 2
+print("Numero de secuencias:", len(ds))
+
+ds = TimeMoEDataset('Time-300B/healthcare/us_births', normalization_method='max')
+assert len(ds) == 1
+print("Numero de secuencias:", len(ds))
+
+ds = TimeMoEDataset('Time-300B/healthcare/project_tycho', normalization_method='max')
+assert len(ds) == 588
+print("Numero de secuencias:", len(ds))
+
+ds = TimeMoEDataset('Time-300B/healthcare/cdc_fluview_ilinet', normalization_method='max')
+assert len(ds) == 286
+print("Numero de secuencias:", len(ds))
+
+ds = TimeMoEDataset('Time-300B/healthcare/cdc_fluview_who_nrevss', normalization_method='max')
+assert len(ds) == 108
+print("Numero de secuencias:", len(ds))
+
+
